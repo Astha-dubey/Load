b26c2ca27cf6b65c22e12356b4091a6004b01a5f
Astha <dubeyastha210@gmail.com>
2023-07-10 21:17:35 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from 'react'
-
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
+import Nav from './Components/Nav'
+import Footer from'./Components/Footer'
+import SignUp from './Components/SignUp';
+// import{BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
-  const [count, setCount ]=useState(0);
-  useEffect(() => {
-    console.warn("useEffect")
-  },[]);
+
   return (
-    <div className='app'>
-
-      <h1>useEffect is React({count})</h1>
-      <button onClick= {()=>setCount(count +1)}>Update counter</button>
-    
-
-
+    <div className='App'>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+<Route path="/" element={<h1>Product Listing Component</h1>}/>
+<Route path="/add" element={<h1>Add Product Component</h1>}/>
+<Route path="/update" element={<h1>Update Product Component</h1>}/>
+<Route path="/logout" element={<h1>Logout Component</h1>}/>
+<Route path="/profile" element={<h1>Profile Component</h1>}/>
+<Route path="/signup" element={<SignUp />}/>
+        </Routes>
+        {/* <h1>E-Dashboard</h1> */}
+        {/* <button onClick= {()=>setCount(count +1)}>Update counter</button> */}
+      </BrowserRouter>
+      <Footer/>
     </div >
   );
 }
